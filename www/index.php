a4f1fb5c1f6d00378f9fdb9aba2c437e1667559b
Sergey-LinuxPC-Git <vodopyanov03@bk.ru>
2024-08-22 20:11:42 +0300

    Настроил docker compose

---
+++ www/index.php
@@ -0,0 +1,13 @@
+<?php 
+
+$user = 'root';
+$pass = 'root';
+
+$dsn = 'mysql:host=mysql2;dbname=information_schema;charset=utf8';
+$pdo = new PDO($dsn, $user, $pass);
+
+$stmt = $pdo->query('select * from tables');
+$row = $stmt->fetch();
+
+print_r($row);
+
